bb850c2cbadb28cb145d30e05c7a990a41fc92b6
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-10-06 20:10:17 -0700

    update readme and clean swap provider code base

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 npm install
 truffle migrate --reset
 // copy the deployed example token address to index.js
+ganache-cli
 node index.js
 ```
--- utils/ERC20SwapProvider.js
+++ utils/ERC20SwapProvider.js
@@ -1,11 +1,27 @@
-// import ERC20Swap from "../contracts/ERC20Swap.json";
 var ERC20Swap = require("../build/contracts/ERC20Swap.json");
 var ExampleCoin = require("../build/contracts/ExampleCoin.json");
-// import ExampleCoin from "../contracts/ExampleCoin.json";
 var truffleContract = require("truffle-contract");
-// import truffleContract from "truffle-contract";
 
-// var artifacts = requirez("../build/contract");
+
+// Convert the JSON artifact to a Contract Object
+async function jsonToContract(json, _web3, accounts) {
+  const contract = truffleContract(json);
+  contract.setProvider(_web3.currentProvider);
+  contract.defaults({
+    from: accounts[0],
+    gas: 4712388,
+    gasPrice: 100000000000
+  })
+  // dirty hack
+  if (typeof contract.currentProvider.sendAsync !== "function") {
+    contract.currentProvider.sendAsync = function() {
+      return contract.currentProvider.send.apply(
+        contract.currentProvider, arguments
+      );
+    };
+  }
+  return contract;
+}
 
 
 class ERC20SwapProvider {
@@ -16,45 +32,16 @@ class ERC20SwapProvider {
 
   async initiateSwap (value, recipientAddress, refundAddress, secretHash, expiration) {
     const web3 = this._web3;
-
     const accounts = await web3.eth.getAccounts();
 
-    const ExampleCoinContract = truffleContract(ExampleCoin);
-    ExampleCoinContract.setProvider(web3.currentProvider);
-    ExampleCoinContract.defaults({
-      from: accounts[0],
-      gas: 4712388,
-      gasPrice: 100000000000
-    })
-    // dirty hack
-    if (typeof ExampleCoinContract.currentProvider.sendAsync !== "function") {
-      ExampleCoinContract.currentProvider.sendAsync = function() {
-        return ExampleCoinContract.currentProvider.send.apply(
-          ExampleCoinContract.currentProvider, arguments
-        );
-      };
-    }
-    // end
+    // Deploy ExampleCoinContract (ERC20 Token)
+    const ExampleCoinContract = await jsonToContract(ExampleCoin, web3, accounts);
     const ExampleCoinInstance = await ExampleCoinContract.deployed();
-
+    
     await ExampleCoinInstance.approve(accounts[0], 9999999, { from: accounts[0] })
 
-    const ERC20SwapContract = truffleContract(ERC20Swap);
-    ERC20SwapContract.setProvider(web3.currentProvider);
-    ERC20SwapContract.defaults({
-      from: accounts[0],
-      gas: 4712388,
-      gasPrice: 100000000000
-    })
-    // dirty hack
-    if (typeof ERC20SwapContract.currentProvider.sendAsync !== "function") {
-      ERC20SwapContract.currentProvider.sendAsync = function() {
-        return ERC20SwapContract.currentProvider.send.apply(
-          ERC20SwapContract.currentProvider, arguments
-        );
-      };
-    }
-    // end
+    // Deploy ERC20SwapContract (Swapper to claim / expire)
+    const ERC20SwapContract = await jsonToContract(ERC20Swap, web3, accounts);
     const ERC20SwapInstance = await ERC20SwapContract.new(
       secretHash,
       expiration,
